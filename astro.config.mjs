bdf626e28763265aaaf56f0277401499c7534f7a
Brendan Annable <brendan@annable.me>
2025-11-21 10:24:35 +1100

    Add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sigenergy.annable.me',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), robotsTxt()],
   compressHTML: false,
   build: {
     format: 'file'
