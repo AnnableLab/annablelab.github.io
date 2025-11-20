4d661a228ff8c6ca6a83605fd5992d11bf05dfb1
Brendan Annable <brendan@annable.me>
2025-11-21 00:41:56 +1100

    Remove summaries

---
+++ _site/astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://sigenergy.annable.me',
+  integrations: [mdx(), sitemap()],
+  build: {
+    format: 'file'
+  },
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+    },
+  },
+  redirects: {
+    '/pages/architecture': '/architecture',
+    '/pages/automation': '/automation',
+    '/pages/conclusion': '/conclusion',
+    '/pages/dashboard': '/dashboard',
+    '/pages/emhass': '/emhass',
+    '/pages/prerequisites': '/prerequisites',
+    '/pages/setup': '/setup',
+  }
+});
+
