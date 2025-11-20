064c53785d05905c0a624cbbcf95cfe4c0d845f2
Brendan Annable <brendan@annable.id.au>
2025-11-20 21:20:35 +1100

    Switch to MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sigenergy.annable.me',
-  integrations: [sitemap()],
+  integrations: [mdx(), sitemap()],
   build: {
     format: 'file'
   }
